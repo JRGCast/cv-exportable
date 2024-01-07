09bf6ce72940e6605a62c02e141b16901712f695
JRGCast <joao.ralph1@gmail.com>
2024-01-07 11:35:59 -0300

    WIP starting work-experience section

--- src/app/page/components/work-experience/work-experience.component.ts
+++ src/app/page/components/work-experience/work-experience.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { mainData } from 'src/app/data/data.config';
+import { IconsSVGNames, getIconAssetInfos } from 'src/app/utils/functions';
 
 @Component({
   selector: 'app-work-experience',
@@ -9,5 +11,9 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./work-experience.component.scss']
 })
 export class WorkExperienceComponent {
-
+  readonly workExperiences = mainData.contact.social.map((socialItem) => ({
+    url: socialItem.url,
+    innerTxt: socialItem.url,
+    ...getIconAssetInfos(socialItem.icon as IconsSVGNames),
+  }));
 }
